52741c0fa2edfb6ac5c90b1ff021f794a302aa02
GGr3ml1N <dinars163@gmail.com>
2020-09-03 19:23:50 +0300

    Возвращено все на круги своя. =)

--- src/main/java/ru/ssau/tk/ggr3ml1n/theyalllookssurprised/HelloWorld.java
+++ src/main/java/ru/ssau/tk/ggr3ml1n/theyalllookssurprised/HelloWorld.java
@@ -3,7 +3,7 @@ package ru.ssau.tk.ggr3ml1n.theyalllookssurprised;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("How are u?");
+        System.out.println("Hello, World!");
     }
 
 }
