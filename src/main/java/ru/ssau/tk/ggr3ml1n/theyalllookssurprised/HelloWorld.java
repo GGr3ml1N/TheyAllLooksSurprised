8e311e1c51a1823d62a1d6c167190e1b72e8c692
GGr3ml1N <dinars163@gmail.com>
2020-09-03 18:54:12 +0300

    Проверка синхрониния

--- src/main/java/ru/ssau/tk/ggr3ml1n/theyalllookssurprised/HelloWorld.java
+++ src/main/java/ru/ssau/tk/ggr3ml1n/theyalllookssurprised/HelloWorld.java
@@ -3,7 +3,7 @@ package ru.ssau.tk.ggr3ml1n.theyalllookssurprised;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("what the weather like today?");
+        System.out.println("How are u?");
     }
 
 }
