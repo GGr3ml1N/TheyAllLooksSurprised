4cd6d082198bbc4990e6244d8e58fa72de0a311d
GGr3ml1N <dinars163@gmail.com>
2020-09-03 18:45:56 +0300

    HelloWorld

---
+++ src/main/java/ru/ssau/tk/ggr3ml1n/theyalllookssurprised/HelloWorld.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.ggr3ml1n.theyalllookssurprised;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
