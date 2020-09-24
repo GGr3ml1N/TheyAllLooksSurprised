4a2f55dd0d347e6efaf9525fa28d065b22cd246a
andrejpenskij <penskiy_a@bk.ru>
2020-09-24 13:31:07 +0400

    создан тест для Identity function

---
+++ src/test/java/function/IdentityFunctionTest.java
@@ -0,0 +1,19 @@
+package function;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class IdentityFunctionTest {
+    private final static double DELTA=0.01;
+    private final MathFunction identityFunction = new IdentityFunction();
+
+    @Test
+    public void testApply() {
+        assertEquals(identityFunction.apply(1.111), 1,DELTA);
+        assertNotEquals(identityFunction.apply(1.111), 7,DELTA);
+        assertNotEquals(identityFunction.apply(1.111), 8,DELTA);
+
+
+    }
+}
