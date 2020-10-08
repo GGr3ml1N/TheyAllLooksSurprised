16f5a140db91bf784d534e4a238674a182598063
GGr3ml1N <dinars163@gmail.com>
2020-10-08 14:08:55 +0300

    Исправлен ConstantFunctionTest

--- src/test/java/ru/ssau/tk/ggr3ml1n/theyalllookssurprised/function/CompositeFunctionTest.java
+++ src/test/java/ru/ssau/tk/ggr3ml1n/theyalllookssurprised/function/CompositeFunctionTest.java
@@ -6,13 +6,14 @@ import static org.testng.Assert.*;
 
 public class CompositeFunctionTest {
     private final static double DELTA = 0.01;
-    public final MathFunction firstFunction = new ReverseSin();
+    private final MathFunction firstFunction = new ReverseSin();
     private final MathFunction secondFunction = new ReverseSin();
     private final MathFunction compositeFunction = new CompositeFunction(firstFunction, secondFunction);
 
     @Test
     public void testApply() {
-        assertEquals(compositeFunction.apply(4.111), -1.07, DELTA);
-
+        assertEquals(compositeFunction.apply(4.111), -1.06, DELTA);
+        assertNotEquals(compositeFunction.apply(1.111), 7, DELTA);
+        assertNotEquals(compositeFunction.apply(1.111), 8, DELTA);
     }
 }
