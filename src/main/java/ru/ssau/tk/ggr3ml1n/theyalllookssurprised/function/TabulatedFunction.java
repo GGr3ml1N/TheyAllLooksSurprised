6a66565f0303a32cefd2f40e442a374aee08cde3
andrejpenskij <penskiy_a@bk.ru>
2020-10-14 19:29:47 +0400

    создан интерфейс TabulatedFunction и методы

---
+++ src/main/java/ru/ssau/tk/ggr3ml1n/theyalllookssurprised/function/TabulatedFunction.java
@@ -0,0 +1,21 @@
+package ru.ssau.tk.ggr3ml1n.theyalllookssurprised.function;
+
+
+
+public interface TabulatedFunction extends MathFunction {
+    int getCount();
+
+    double getX(int index);
+
+    double getY(int index);
+
+    void setY(int index, double value);
+
+    int indexOfX(double x);
+
+    int indexOfY(double y);
+
+    double leftBound();
+
+    double rightBound();
+}
