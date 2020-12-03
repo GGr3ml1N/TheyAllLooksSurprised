b0f82052769ab3a26660905eb75efa99c8977689
andrejpenskij <penskiy_a@bk.ru>
2020-12-03 20:36:31 +0400

    2.8 создан метод и тесты к нему

--- src/main/java/ru/ssau/tk/ggr3ml1n/theyalllookssurprised/function/CompositeFunction.java
+++ src/main/java/ru/ssau/tk/ggr3ml1n/theyalllookssurprised/function/CompositeFunction.java
@@ -2,17 +2,17 @@ package ru.ssau.tk.ggr3ml1n.theyalllookssurprised.function;
 
 public class CompositeFunction implements MathFunction {
 
-    private final MathFunction firstFunction;
+    private final MathFunction k_firstFunction;
     private final MathFunction secondFunction;
 
     public CompositeFunction(MathFunction firstFunction, MathFunction secondFunction) {
-        this.firstFunction = firstFunction;
+        this.k_firstFunction = firstFunction;
         this.secondFunction = secondFunction;
     }
 
     @Override
     public double apply(double x) {
-        return secondFunction.apply(firstFunction.apply(x));
+        return secondFunction.apply(k_firstFunction.apply(x));
     }
 
 }
