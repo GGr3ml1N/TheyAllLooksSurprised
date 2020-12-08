610d7dc97d6a777af970d686a73e2c4ec493ea44
andrejpenskij <penskiy_a@bk.ru>
2020-12-08 11:29:45 +0400

    ЛР 3.1 Реализация метода iterator() в ArrayTabulatedFunction

--- src/main/java/ru/ssau/tk/ggr3ml1n/theyalllookssurprised/function/ArrayTabulatedFunction.java
+++ src/main/java/ru/ssau/tk/ggr3ml1n/theyalllookssurprised/function/ArrayTabulatedFunction.java
@@ -4,6 +4,7 @@ import ru.ssau.tk.ggr3ml1n.theyalllookssurpriswd.exeptions.InterpolationExceptio
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class ArrayTabulatedFunction extends AbstractTabulatedFunction {
     protected int count;
@@ -125,6 +126,23 @@ public class ArrayTabulatedFunction extends AbstractTabulatedFunction {
 
     @Override
     public Iterator<Point> iterator() {
-        throw new UnsupportedOperationException();
+        return new Iterator() {
+            int i = 0;
+
+            @Override
+            public boolean hasNext() {
+                return i < count;
+            }
+
+            @Override
+            public Point next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                Point point = new Point(xValues[i], yValues[i]);
+                i++;
+                return point;
+            }
+        };
     }
 }
