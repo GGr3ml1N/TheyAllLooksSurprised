07c760a44beaa02fbe86d616cce35918cfe67463
GGr3ml1N <dinars163@gmail.com>
2021-02-17 18:30:25 +0300

    Y Объекты класса SynchronizedTabulatedFunction обладают тем недос…
    …татком, что вызов каждого метода производится в своём блоке синхронизации.
    
          Т.е., например, между вызовами двух методов одним потоком исполнения может успеть «влезть» другой поток.
          Требуется написать метод, который бы позволял совершать набор действий с объектом этого класса внутри одного блока синхронизации.
    
          Для этого нужно создать внутренний публичный интерфейс операции Operation с типом-параметром T, характеризующий возвращаемое значение операции (если возвращаемое значение не нужно, можно будет подставлять вместо T тип Void, а в конце реализации возвращать null).
    
          Интерфейс должен содержать единственный метод apply(), принимающий в качестве аргумента SynchronizedTabulatedFunction и возвращающий T.
    
          Добавить в класс SynchronizedTabulatedFunction метод doSynchronously() с типом-параметром T, принимающий на вход операцию Operation и возвращающий T. Подумать, чем именно приходится операция для параметра T (тип возвращаемого значения) – производителем или потребителем – и на основании этого записать в Operation<…> правильный подстановочный символ (wildcard). Метод должен выполнять указанную операцию, возвращать значение, и всё это должно быть внутри блока синхронизации.
    
          Покрыть метод тестами в однопоточной среде для различных типов Operation<T> (T в том числе Void). Для этого можно использовать анонимные классы или лямбда-выражения.

--- src/main/java/ru/ssau/tk/ggr3ml1n/theyalllookssurprised/concurrent/SynchronizedTabulatedFunction.java
+++ src/main/java/ru/ssau/tk/ggr3ml1n/theyalllookssurprised/concurrent/SynchronizedTabulatedFunction.java
@@ -9,6 +9,7 @@ import java.util.NoSuchElementException;
 import java.util.Objects;
 
 public class SynchronizedTabulatedFunction implements TabulatedFunction {
+
     private final TabulatedFunction tabulatedFunction;
     private final Object mutex;
 
@@ -17,74 +18,74 @@ public class SynchronizedTabulatedFunction implements TabulatedFunction {
         this.mutex = Objects.requireNonNull(mutex);
     }
 
+    public interface Operation<T> {
+        T apply(SynchronizedTabulatedFunction synchronizedTabulatedFunction);
+    }
+
+    public <T> T doSynchronously(Operation<? extends T> operation) {
+        synchronized (mutex) {
+            return operation.apply(this);
+        }
+    }
+
     @Override
     public int getCount() {
         synchronized (mutex) {
             return tabulatedFunction.getCount();
         }
     }
-
     @Override
     public double getX(int index) {
         synchronized (mutex) {
             return tabulatedFunction.getX(index);
         }
     }
-
     @Override
     public double getY(int index) {
         synchronized (mutex) {
             return tabulatedFunction.getY(index);
         }
     }
-
     @Override
     public void setY(int index, double value) {
         synchronized (mutex) {
             tabulatedFunction.setY(index, value);
         }
     }
-
     @Override
     public int indexOfX(double x) {
         synchronized (mutex) {
             return tabulatedFunction.indexOfX(x);
         }
     }
-
     @Override
     public int indexOfY(double y) {
         synchronized (mutex) {
             return tabulatedFunction.indexOfY(y);
         }
     }
-
     @Override
     public double leftBound() {
         synchronized (mutex) {
             return tabulatedFunction.leftBound();
         }
     }
-
     @Override
     public double rightBound() {
         synchronized (mutex) {
             return tabulatedFunction.rightBound();
         }
     }
-
     @Override
     public Iterator<Point> iterator() {
         synchronized (mutex) {
             Point[] points = TabulatedFunctionOperationService.asPoints(tabulatedFunction);
             return new Iterator<Point>() {
                 int i = 0;
-
                 @Override
                 public boolean hasNext() {
                     return i < points.length;
                 }
-
                 @Override
                 public Point next() {
                     if (!hasNext()) {
@@ -95,7 +96,6 @@ public class SynchronizedTabulatedFunction implements TabulatedFunction {
             };
         }
     }
-
     @Override
     public double apply(double x) {
         synchronized (mutex) {
